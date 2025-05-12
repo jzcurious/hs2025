f2abd2c023f70ef7e0b379ea15b8fdd48c9f0b88
jzcurious <paranoid.hpc.io@gmail.com>
2025-05-12 10:53:19 +0700

    add CUDATimer

--- work1/benchmarks/src/benchmarks.cpp
+++ work1/benchmarks/src/benchmarks.cpp
@@ -18,6 +18,31 @@ static void BM_EigenVectorAddCPU(benchmark::State& state) {
   }
 }
 
+class CUDATimer {
+ private:
+  float& _elapse_time_s;
+  cudaEvent_t _start, _stop;
+
+ public:
+  CUDATimer(float& elapse_time_s)
+      : _elapse_time_s(elapse_time_s) {
+    cudaEventCreate(&_start);
+    cudaEventCreate(&_stop);
+
+    cudaEventRecord(_start);
+  }
+
+  ~CUDATimer() {
+    cudaEventRecord(_stop);
+    cudaEventSynchronize(_stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, _start, _stop);
+
+    _elapse_time_s = milliseconds / 1000;
+  }
+};
+
 static void BM_OurVectorAddGPU(benchmark::State& state) {
   int len = state.range(0);
 
@@ -35,23 +60,51 @@ static void BM_OurVectorAddGPU(benchmark::State& state) {
   cudaMemcpy(d_a, b.data(), size, cudaMemcpyHostToDevice);
 
   for (auto _ : state) {
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+    float elapsed_time = 0;
 
-    cudaEventRecord(start);
-    w1::vadd_f32(d_a, d_b, d_c, len);
-    cudaEventRecord(stop);
+    {
+      CUDATimer timer(elapsed_time);
+      w1::vadd_f32(d_a, d_b, d_c, len);
+    }
 
-    cudaEventSynchronize(stop);
-
-    float milliseconds = 0;
-    cudaEventElapsedTime(&milliseconds, start, stop);
-
-    benchmark::DoNotOptimize(milliseconds);
+    benchmark::DoNotOptimize(elapsed_time);
     benchmark::ClobberMemory();
 
-    state.SetIterationTime(milliseconds / 1000);
+    state.SetIterationTime(elapsed_time);
+  }
+
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+}
+
+static void BM_OurVectorAddGPUCopyOverhead(benchmark::State& state) {
+  int len = state.range(0);
+
+  Eigen::VectorXf a = Eigen::VectorXf::Random(len);
+  Eigen::VectorXf b = Eigen::VectorXf::Random(len);
+
+  float *d_a, *d_b, *d_c;
+  auto size = len * sizeof(float);
+
+  cudaMalloc(&d_a, size);
+  cudaMalloc(&d_b, size);
+  cudaMalloc(&d_c, size);
+
+  for (auto _ : state) {
+    float elapsed_time = 0;
+
+    {
+      CUDATimer timer(elapsed_time);
+      cudaMemcpy(d_a, a.data(), size, cudaMemcpyHostToDevice);
+      cudaMemcpy(d_a, b.data(), size, cudaMemcpyHostToDevice);
+      w1::vadd_f32(d_a, d_b, d_c, len);
+    }
+
+    benchmark::DoNotOptimize(elapsed_time);
+    benchmark::ClobberMemory();
+
+    state.SetIterationTime(elapsed_time);
   }
 
   cudaFree(d_a);
